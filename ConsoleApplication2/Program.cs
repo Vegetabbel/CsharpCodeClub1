cd342a3b218fdb8ec8bb7414ed8ca9af26c76fd3
Vegetabbel <veikkahuttunen@kamk.fi>
2015-09-16 17:05:53 +0300

    Start

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Player
+    {
+        private string name = "";
+        private int health = 100;
+        private int mana = 100;
+        private int stamina = 100;
+    }
+}
